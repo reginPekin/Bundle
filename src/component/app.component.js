a23cd775be0ab094388868b9d86e6370f93dd484
reginPekin <reginbegin@gmail.com>
2020-02-04 15:39:04 +0300

    tree shaking checking

--- src/component/app.component.js
+++ src/component/app.component.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import styles from "./app.component.css";
 import moment from "moment";
+import { cube } from "../math";
 
 export const App = () => {
   const [backColor, setBackColor] = useState(true);
@@ -18,6 +19,7 @@ export const App = () => {
       }
     >
       <span>Hello World! Today: {now.format("dddd, MMMM DD YYYY")}</span>
+      <span>The cube of 3 is {cube(3)}</span>
       <button onClick={() => setBackColor(!backColor)}>
         Change the color!
       </button>
